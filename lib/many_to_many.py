388f47221ee762aa93017aca84b1d1e374b1777c
morriskaria <morriskaria542@gmail.com>
2025-08-20 14:32:16 +0300

    commit

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,104 @@
-class Author:
-    pass
-
-
+# lib/many_to_many.py
 class Book:
-    pass
+    all_books = []
+
+    def __init__(self, title):
+        self.title = title
+        self.__class__.all_books.append(self)
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+
+
+class Author:
+    all_authors = []
+
+    def __init__(self, name):
+        self.name = name
+        self.__class__.all_authors.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        if not isinstance(book, Book):
+            raise Exception("book must be an instance of Book")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an integer")
+        
+        contract = Contract(self, book, date, royalties)
+        return contract
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Contract:
-    pass
+    all = []  # Changed to match what the test expects
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise Exception("book must be an instance of Book")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an integer")
+        
+        self._author = author
+        self._book = book
+        self._date = date
+        self._royalties = royalties
+        self.__class__.all.append(self)
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
+    
+    # Property getters and setters with validation
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception("author must be an instance of Author")
+        self._author = value
+    
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, value):
+        if not isinstance(value, Book):
+            raise Exception("book must be an instance of Book")
+        self._book = value
+    
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise Exception("date must be a string")
+        self._date = value
+    
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, value):
+        if not isinstance(value, int):
+            raise Exception("royalties must be an integer")
+        self._royalties = value
